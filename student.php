e4a4b9f1ea1ba4cbccb3fd4acdf8a21680b6644f
akif_abrar <akifabrar54370@gmail.com>
2026-05-05 08:51:40 +0600

    Final Lab Task 03

---
+++ student.php
@@ -0,0 +1,13 @@
+<?php
+
+$student = array(
+    "name" => "Abrar Ul Karim",
+    "id" => "221-15-5678",
+    "department" => "Computer Science and Engineering",
+    "cgpa" => "3.85"
+);
+
+echo json_encode($student);
+
+?>
+
